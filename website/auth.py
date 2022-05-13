443b702f0048a40ad2af0daf65d64776cf60bd13
benedito <benethowen@yahoo.com.br>
2022-05-12 23:21:46 -0300

    Completado o sistema de login, sing-up e logout com as permssões e sessões de acesso.

--- website/auth.py
+++ website/auth.py
@@ -1,8 +1,12 @@
 from unicodedata import category
 from flask import Blueprint, render_template, request, flash, redirect, url_for
+from sqlalchemy import null
 from .models import User
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, login_required, logout_user, current_user 
+
+
 
 auth = Blueprint('auth', __name__)
 
@@ -10,11 +14,28 @@ auth = Blueprint('auth', __name__)
 def login():
     data = request.form
     print(data)
-    return render_template('login.html')
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                 flash('Logado com sucesso', category='success')
+                 login_user(user, remember=True)
+                 return redirect(url_for('views.home'))
+            else:
+                flash('Incorreto password, tente novamente!', category='error')
+        else:
+            flash('Email não existente', category='error')
+       
+    return render_template('login.html', user=current_user)
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "esta é logout page"
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
@@ -25,6 +46,10 @@ def sign_up():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
+        user = User.query.filter_by(email=email).first()
+
+        if user:
+            flash('Email deve ser informado', category='error')
         if len(email)< 4:
             flash('email deve ter mais de 4 caracteres', category='error')
         elif len(nome) < 2:
@@ -38,9 +63,10 @@ def sign_up():
             new_user = User(email=email, nome=nome, password=generate_password_hash(password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
+            login_user(user, remember=True)
             flash('conta criado com sucesso!', category='success')
             return redirect(url_for('views.home'))
 
 
 
-    return render_template('sign_up.html')
+    return render_template('sign_up.html', user=current_user)
