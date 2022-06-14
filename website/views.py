69420a73868e9cc44c007522bce8fe128b295d15
benedito <benethowen@yahoo.com.br>
2022-06-14 20:41:39 -0300

    Modificando provisóriamente a pasta models para resolver bugs

--- website/views.py
+++ website/views.py
@@ -2,6 +2,7 @@ from unicodedata import category
 from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user 
 from .models import Note
+
 from . import db
 import json
 
