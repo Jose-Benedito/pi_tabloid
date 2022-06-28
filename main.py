e9580d0a3f36f9ac3b085c56aa229970c5cf157a
jose-benedito <benethowen@yahoo.com.br>
2022-06-28 16:15:38 -0300

    code

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ import os
 app = create_app()
 
 if __name__ == '__main__':
-    port = int(os.getenv('PORT'), '5000')
-    app.run(host='0.0.0.', port = port)
+    #port = int(os.getenv('PORT'), '5000')
+    app.run()
     
