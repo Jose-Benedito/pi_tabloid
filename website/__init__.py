4362717967cba0ebf8619f5635858f9bceea5c50
jose-benedito <benethowen@yahoo.com.br>
2022-06-30 18:38:20 -0300

    Configurações do postgres

--- website/__init__.py
+++ website/__init__.py
@@ -18,7 +18,7 @@ def create_app():
     app.config['SECRET_KEY'] = 'grhteyeuwhhs fgdhjajakuww'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
  #Postgres
-    #app.config['SQLALCHEMY_DATABASE_URI']=f'postgresql://{USER}:{PASSWORD}{HOST}/{DB_POSTGRES}'
+    app.config['SQLALCHEMY_DATABASE_URI']=f'postgresql://{USER}:{PASSWORD}@{HOST}:5432/{DB_POSTGRES}'
     db.init_app(app)
 
 
@@ -44,7 +44,7 @@ def create_app():
         
     return app
 def create_database(app):
-    #db.create_all(app=app)
+    db.create_all(app=app)
     if not path.exists('website/' + DB_NAME):
        db.create_all(app=app)
        print('Created Database')
